8c3b4df63a86b84bf4889e1a84e8e732b99b52bd
msilis <mikssilis@gmail.com>
2023-11-15 22:25:31 +0000

    add jest config

--- src/app/dashboard/dashboardUtils.test.ts
+++ src/app/dashboard/dashboardUtils.test.ts
@@ -1,26 +1,21 @@
 import { handleLogout } from "./dashboardUtils";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { mock } from "jest-mock-extended";
+
+// Mock the router object
+const mockRouter = mock<AppRouterInstance>();
+
+// Mock the sessionStorage object
+global.sessionStorage = mock<Storage>();
 
 describe("handleLogout", () => {
-  it('should remove email from sessionStorage and redirect to "/"', () => {
-    const mockRouter = {
-      push: jest.fn(),
-    };
-
-    sessionStorage.setItem("email", "test@example.com");
+  it("should remove email from sessionStorage and redirect to home page", () => {
     handleLogout(mockRouter);
 
-    expect(sessionStorage.getItem("email")).toBeNull();
-    expect(mockRouter.push).toHaveBeenCalledWith("/");
-  });
+    // Verify that removeItem was called with 'email'
+    expect(sessionStorage.removeItem).toHaveBeenCalledWith("email");
 
-  it('should not remove email from sessionStorage if it does not exist and redirect to "/"', () => {
-    const mockRouter = {
-      push: jest.fn(),
-    };
-
-    handleLogout(mockRouter);
-
-    expect(sessionStorage.getItem("email")).toBeNull();
+    // Verify that push was called with '/'
     expect(mockRouter.push).toHaveBeenCalledWith("/");
   });
 });
