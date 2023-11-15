568ea62de8cf439326fa234e00b1122702cf6bce
msilis <mikssilis@gmail.com>
2023-11-15 11:27:28 +0000

    auth route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,13 @@
+import { isAuthenticated } from "../../../utils/Auth";
+import { redirect } from "next/navigation";
+
 const Dashboard = () => {
+  const isAuth = isAuthenticated;
+
+  if (!isAuth) {
+    redirect("/");
+  }
+
   return (
     <div className="text-center bg-slate-200">
       <h2>Dashboard</h2>
