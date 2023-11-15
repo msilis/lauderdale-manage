0da8d3ffeac3f322b52b009d576ff8d3aae5d7c6
msilis <mikssilis@gmail.com>
2023-11-15 13:04:42 +0000

    add redirect to dashboard after login

--- src/components/loginForm/loginForm.tsx
+++ src/components/loginForm/loginForm.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import { handleLogin } from "./loginUtils";
 import { useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export const LoginForm = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
+  const router = useRouter();
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (email && password) {
+      handleLogin(email, password, router);
+    }
+  };
+
   return (
-    <form onSubmit={handleLogin}>
+    <form onSubmit={handleFormSubmit}>
       <div>
         <label htmlFor="email">Email:</label>
         <input
-          type="text"
+          type="email"
           id="email"
           className="input input-bordered w-full max-w-xs"
           ref={emailRef}
--- src/components/loginForm/loginUtils.ts
+++ src/components/loginForm/loginUtils.ts
@@ -1,3 +1,22 @@
-export const handleLogin = () => {
-  return;
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+export const handleLogin = async (
+  email: string,
+  password: string,
+  router: string[] | AppRouterInstance
+) => {
+  console.log(email, password);
+  const response = await fetch("../../api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  if (!response.ok) {
+    console.error("Login failed");
+    return;
+  }
+  console.log("Login successful!");
+  router.push("/dashboard");
 };
