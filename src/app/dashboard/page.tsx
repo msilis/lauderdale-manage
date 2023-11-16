3fd1555667d765e4f8442abca5a03ff3d9219a98
msilis <mikssilis@gmail.com>
2023-11-16 09:39:28 +0000

    remove unused imports from dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import { DashboardHeader } from "@/components/dashboardHeader/dashboardHeader";
 import { isAuthenticated } from "../../../utils/Auth";
 import { redirect } from "next/navigation";
-import { handleLogout } from "./dashboardUtils";
-import { useRouter } from "next/navigation";
 import { Sidebar } from "@/components/sidebar/sidebar";
 
 const Dashboard = () => {
-  const router = useRouter();
   const isAuth = isAuthenticated;
 
   if (!isAuth) {
