54b95e85ee70286dabf4370719c2a2b3f7e9f832
msilis <mikssilis@gmail.com>
2023-11-16 12:09:07 +0000

    remove unused imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { isAuthenticated } from "../../../utils/Auth";
-import { redirect, useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { componentBasedOnRoute } from "./componentBasedOnRoute";
 import DashboardLayout from "./dashboardLayout";
 
 const Dashboard = () => {
-  const isAuth = isAuthenticated;
-  const router = useRouter();
   const pathname = usePathname();
 
   const Component = componentBasedOnRoute(pathname);
