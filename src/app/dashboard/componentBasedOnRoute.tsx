26ce207958b64ca200475259ae0e4afa037dab9d
msilis <mikssilis@gmail.com>
2023-11-16 10:19:55 +0000

    add dynamic routing

--- src/app/dashboard/componentBasedOnRoute.tsx
+++ src/app/dashboard/componentBasedOnRoute.tsx
@@ -4,3 +4,25 @@ import Students from "./students/page";
 import Families from "./families/page";
 import Teachers from "./teachers/page";
 import Classes from "./classes/page";
+
+export const componentBasedOnRoute = (pathname: string) => {
+  let Component;
+  switch (pathname) {
+    case "/dashboard/students":
+      Component = Students;
+      break;
+    case "dashboard/families":
+      Component = Families;
+      break;
+    case "/dashboard/Teachers":
+      Component = Teachers;
+      break;
+    case "dashboard/classes":
+      Component = Classes;
+      break;
+    default:
+      Component = Overview;
+  }
+
+  return Component;
+};
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { isAuthenticated } from "../../../utils/Auth";
-import { redirect } from "next/navigation";
+import { redirect, useRouter, usePathname } from "next/navigation";
 import { Sidebar } from "@/components/sidebar/sidebar";
+import { componentBasedOnRoute } from "./componentBasedOnRoute";
 
 const Dashboard = () => {
   const isAuth = isAuthenticated;
+  const router = useRouter();
+  const pathname = usePathname();
 
   if (!isAuth) {
     redirect("/");
@@ -17,12 +20,16 @@ const Dashboard = () => {
     loginEmail = sessionStorage.getItem("email");
   }
 
+  const Component = componentBasedOnRoute(pathname);
+
   return (
     <div className="grid grid-cols-3">
       <div>
         <Sidebar />
       </div>
-      <div></div>
+      <div>
+        <Component />
+      </div>
       <div></div>
     </div>
   );
