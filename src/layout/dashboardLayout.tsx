13ef4d5774ea0b929def0cdcf198055db43a0857
msilis <mikssilis@gmail.com>
2023-11-17 22:04:34 +0000

    add navbar and resize grid

--- src/layout/dashboardLayout.tsx
+++ src/layout/dashboardLayout.tsx
@@ -15,11 +15,11 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     loginEmail = sessionStorage.getItem("email");
   }
   return (
-    <div className="grid grid-cols-3">
-      <div>
+    <div className="grid grid-cols-[10%_auto_auto]">
+      <div className="w-1/5">
         <Sidebar />
       </div>
-      <div className="mt-6">{children}</div>
+      <div className="col-span-2 mt-6 mr-8">{children}</div>
       <div></div>
     </div>
   );
