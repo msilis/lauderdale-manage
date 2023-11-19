89c34841e0e7bf8341413e9eccb40f7fcef33967
msilis <mikssilis@gmail.com>
2023-11-19 08:36:46 +0000

    create family view

---
+++ src/app/dashboard/families/familyView/familyView.tsx
@@ -0,0 +1,9 @@
+const FamilyView = () => {
+  return (
+    <div className="flex flex-col">
+      <h3 className="font-bold">Current Families</h3>
+    </div>
+  );
+};
+
+export default FamilyView;
--- src/app/dashboard/families/page.tsx
+++ src/app/dashboard/families/page.tsx
@@ -1,15 +1,17 @@
 import { UI_TEXT } from "../../../../utils/uitext";
 import DashboardLayout from "../../../layout/dashboardLayout";
 import Navbar from "@/components/navbar/navbar";
+import FamilyView from "./familyView/familyView";
 
 const Families = () => {
   return (
-    <div className="flex flex-col ml-28">
+    <div className="flex flex-col ml-28 gap-6">
       <h1 className="text-5xl font-bold">Families</h1>
       <Navbar
         buttonText={UI_TEXT.addFamily}
         url="/dashboard/families/addfamilies"
       />
+      <FamilyView />
     </div>
   );
 };
