eded0d288acbd30eda821ac113f8e3ba7efa84b8
msilis <mikssilis@gmail.com>
2023-11-20 11:44:26 +0000

    toast text enum

--- src/components/toast/toast.ts
+++ src/components/toast/toast.ts
@@ -12,3 +12,16 @@ export const successToast = (toastText: string) => {
     theme: "light",
   });
 };
+
+export const errorToast = (toastText: string) => {
+  return toast.error(toastText, {
+    position: "top-right",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
