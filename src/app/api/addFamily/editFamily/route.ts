a28564a0df03e07db754c0253227b9565481ca24
msilis <mikssilis@gmail.com>
2023-11-24 09:54:21 +0000

    update family route

--- src/app/api/addFamily/editFamily/route.ts
+++ src/app/api/addFamily/editFamily/route.ts
@@ -1,3 +1,19 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase/firebaseConfig";
 import { doc, updateDoc } from "firebase/firestore";
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.text();
+    const { id, ...data } = JSON.parse(body);
+    const familyRef = doc(db, "families", id);
+    await updateDoc(familyRef, data);
+    return NextResponse.json({ message: "Family updated" }, { status: 200 });
+  } catch (error) {
+    console.error("There was an error updating the family", error);
+    return NextResponse.json(
+      { message: "There was an error updating the family" },
+      { status: 500 }
+    );
+  }
+}
