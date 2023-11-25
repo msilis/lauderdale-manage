eff0f3a8ec509c760ef0fa53cbd0eef6c3d94cbd
msilis <mikssilis@gmail.com>
2023-11-25 21:10:54 +0000

    remove console log

--- src/app/dashboard/classes/[classId]/classStudentDisplay.tsx
+++ src/app/dashboard/classes/[classId]/classStudentDisplay.tsx
@@ -24,7 +24,6 @@ const ClassStudentDisplay = ({ classDetail }: { classDetail: ClassData }) => {
     }
   }, [addStudent]);
 
-  console.log(students);
   return (
     <div className="flex flex-col ml-5 gap-6">
       <h3 className="font-bold">Students</h3>
