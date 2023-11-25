cc4077a1c6fad18a02ee2ac2624e805c19edcaf3
msilis <mikssilis@gmail.com>
2023-11-25 22:02:59 +0000

    change navbar to be able to take onClick

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   buttonText: string;
   url: string;
   className?: string;
+  onClick?: () => void;
 };
 
 type NavbarProps = {
@@ -14,18 +15,28 @@ type NavbarProps = {
 
 const Navbar: React.FC<NavbarProps> = ({ buttons }) => {
   const router = useRouter();
-  const handleButtonClick = (url: string) => {
-    router.push(url);
+  const handleButtonClick = (url: string, onClick?: () => void) => {
+    if (onClick) {
+      onClick();
+    } else {
+      router.push(url);
+    }
   };
   return (
     <div className="navbar bg-base-100 outline outline-gray-50 mt-2 drop-shadow-xl md:max-md:w-72 overflow-auto">
       {buttons &&
         buttons.map(
-          ({ buttonText, url, className = "btn btn-ghost text-l" }, index) => (
+          (
+            { buttonText, url, className = "btn btn-ghost text-l", onClick },
+            index
+          ) => (
             <button
               key={index}
               className={`${className}`}
-              onClick={() => handleButtonClick(url)}
+              onClick={(event) => {
+                event.preventDefault();
+                handleButtonClick(url, onClick);
+              }}
             >
               {buttonText}
             </button>
