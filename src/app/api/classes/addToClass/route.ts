88b6131affb2738b51ff35647cf112569d501506
msilis <mikssilis@gmail.com>
2023-11-27 10:42:45 +0000

    merge data into database

--- src/app/api/classes/addToClass/route.ts
+++ src/app/api/classes/addToClass/route.ts
@@ -1,11 +1,24 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase/firebaseConfig";
 import { doc, setDoc } from "firebase/firestore";
+import firebase from "firebase/compat/app";
 
 export async function POST(request: Request) {
   try {
     const body = await request.text();
-    const { id, ...data } = JSON.parse(body);
+    const { id, ...studentData } = JSON.parse(body);
+    const classRef = doc(db, "classes", id);
+    await setDoc(
+      classRef,
+      {
+        classStudents: firebase.firestore.FieldValue.arrayUnion(...studentData),
+      },
+      { merge: true }
+    );
+    return NextResponse.json(
+      { message: "Students added to class" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error("There was an error adding students to class");
     return NextResponse.json(
