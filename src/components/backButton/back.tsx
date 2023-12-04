1f941c89c48e652604b81c946f4942cf39c68b5d
msilis <mikssilis@gmail.com>
2023-12-04 10:35:24 +0000

    create back button component

---
+++ src/components/backButton/back.tsx
@@ -0,0 +1,9 @@
+export const BackButton = () => {
+  return (
+    <div>
+      <button className="btn btn-ghost">
+        <img src="/Feather-arrows-chevron-left.svg" />
+      </button>
+    </div>
+  );
+};
