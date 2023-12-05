276fc3355612ba76fc5d12c58a02d1cd9594a226
msilis <mikssilis@gmail.com>
2023-12-05 22:50:19 +0000

    type TermDates

--- src/app/dashboard/settings/termDateView/termDateViewUtils.ts
+++ src/app/dashboard/settings/termDateView/termDateViewUtils.ts
@@ -1,7 +1,13 @@
 import { errorToast } from "@/components/toast/toast";
 import { TOAST_TEXT } from "@/components/toast/toastText";
 
-const convertDate = (date) => {
+type TermDates = {
+  id: string;
+  field: number[];
+  termDates: Date[];
+};
+
+const convertDate = (date: TermDates) => {
   let sortedDates;
   if (date && Array.isArray(date)) {
     const mappedDates = date.map((unixdate: number) => new Date(unixdate));
@@ -25,6 +31,7 @@ export const fetchTermDates = async () => {
     }
     const termDateData = await response.json();
     const lastTermDate = termDateData.pop();
+    console.log(lastTermDate, "lastTermDate");
     const convertedDates = convertDate(lastTermDate.termDates);
 
     return convertedDates;
