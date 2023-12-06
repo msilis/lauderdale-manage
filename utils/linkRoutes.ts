f65c6d7c39ce2eb99f5f44ac5ac9144ea9092984
msilis <mikssilis@gmail.com>
2023-12-06 11:54:47 +0000

    refactor addStudents and use link_route

--- utils/linkRoutes.ts
+++ utils/linkRoutes.ts
@@ -2,4 +2,5 @@ export const LINK_ROUTE = {
   addClass: "/dashboard/classes/addClass",
   addFamilies: "/dashboard/families/addfamilies",
   setTermDates: "/dashboard/settings/setTermDates",
+  addStudents: "/dashboard/students/addstudents",
 };
