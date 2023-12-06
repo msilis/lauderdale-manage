2ac545f1169d023599743d54e145139c1a952d1e
msilis <mikssilis@gmail.com>
2023-12-06 11:45:03 +0000

    use linkroute in addClass

--- src/app/dashboard/classes/page.tsx
+++ src/app/dashboard/classes/page.tsx
@@ -4,13 +4,14 @@ import { UI_TEXT } from "../../../../utils/uitext";
 import ClassView from "./classView/classView";
 import Link from "next/link";
 import { STYLE_UTILS } from "../../../../utils/styleUtils";
+import { LINK_ROUTE } from "../../../../utils/linkRoutes";
 
 const Classes = () => {
   return (
     <div className="flex flex-col ml-28 gap-6">
       <h1 className="text-5xl font-bold">Classes</h1>
       <Navbar className={STYLE_UTILS.navbarStyle}>
-        <Link href="/dashboard/classes/addClass">
+        <Link href={LINK_ROUTE.addClass}>
           <button className={STYLE_UTILS.ghostButton}>
             {UI_TEXT.addClassButton}
           </button>
