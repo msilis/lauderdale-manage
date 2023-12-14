194498be226bfcff10db587669cece0f1838fb38
msilis <mikssilis@gmail.com>
2023-12-14 10:34:36 +0000

    fill in class list table

--- src/app/dashboard/classes/generateClassList/generatedTable.tsx
+++ src/app/dashboard/classes/generateClassList/generatedTable.tsx
@@ -12,23 +12,49 @@ export const GeneratedTable: React.FC<GeneratedTableProps> = ({
   termDates,
   halfTermDates,
 }) => {
+  console.log(classDetail);
   return (
     <div>
-      <table className="table">
+      <table className="table outline outline-[1px] outline-slate-800 rounded-none">
         <thead>
-          <tr>
+          <tr className="outline outline-[1px] bg-slate-100 font-bold">
             <th>{TABLE_TEXT.studentFirstName}</th>
-            <th>{TABLE_TEXT.studentLastName}</th>
             <th>{TABLE_TEXT.studentBirthday}</th>
             <th>{TABLE_TEXT.studentTeacher}</th>
             <th>{TABLE_TEXT.parent1FirstName}</th>
             <th>{TABLE_TEXT.parent2FirstName}</th>
             {termDates &&
-              termDates[0].map((date: Date) => (
+              termDates[1].map((date: Date) => (
                 <th>{`${date.getDate()}/${date.getMonth() + 1}`}</th>
               ))}
           </tr>
         </thead>
+        <tbody>
+          {classDetail &&
+            classDetail.classStudents?.map((student) => (
+              <tr>
+                <td className="outline outline-[0.5px] outline-slate-500">
+                  {student.studentName}
+                </td>
+                <td className="outline outline-[1px] outline-slate-500">
+                  Birthday
+                </td>
+                <td className="outline outline-[1px] outline-slate-500">
+                  Teacher
+                </td>
+                <td className="outline outline-[1px] outline-slate-500">
+                  Parent 1
+                </td>
+                <td className="outline outline-[1px] outline-slate-500">
+                  Parent2
+                </td>
+                {termDates &&
+                  termDates[1].map(() => (
+                    <td className="outline outline-[1px] outline-slate-500"></td>
+                  ))}
+              </tr>
+            ))}
+        </tbody>
       </table>
     </div>
   );
