1259e8a39df04234490289d91ba308cf81b9fef1
msilis <mikssilis@gmail.com>
2023-12-18 21:50:34 +0000

    expand student object in api

--- src/app/api/classes/addToClass/route.ts
+++ src/app/api/classes/addToClass/route.ts
@@ -2,17 +2,24 @@ import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase/firebaseConfig";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 
+type SaveStudentProps = {
+  id: string;
+  label: string;
+  studentBirthdate: string;
+  parent1FirstName?: string;
+  parent2FirstName?: string;
+  studentTeacher?: string;
+};
+
 export async function POST(request: Request) {
   try {
     const body = await request.text();
     const { classId, selectedStudents } = JSON.parse(body);
-    const studentsToAdd = selectedStudents.map(
-      (student: { id: string; label: string; studentBirthdate: string }) => ({
-        studentId: student.id,
-        studentName: student.label,
-        studentBirthdate: student.studentBirthdate,
-      })
-    );
+    const studentsToAdd = selectedStudents.map((student: SaveStudentProps) => ({
+      studentId: student.id,
+      studentName: student.label,
+      studentBirthdate: student.studentBirthdate,
+    }));
 
     const classRef = doc(db, "classes", classId);
     const classDoc = await getDoc(classRef);
