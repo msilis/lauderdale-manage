40dc80083b1c109497ef2799f959b61bfdca2fb9
msilis <mikssilis@gmail.com>
2024-01-04 03:37:48 +0000

    finish route to get student info

--- src/app/api/classes/getClassStudentsForTable/route.ts
+++ src/app/api/classes/getClassStudentsForTable/route.ts
@@ -1,5 +1,24 @@
 import { NextResponse } from "next/server";
 import { db } from "@/lib/firebase/firebaseConfig";
-import { getDocs } from "firebase/firestore";
+import { collection, doc, getDoc } from "firebase/firestore";
 
-export async function POST(request: Request) {}
+export async function POST(request: Request) {
+  try {
+    const studentsToGet = await request.json();
+    const studentPromises = studentsToGet.map(
+      (student: { studentId: string }) => {
+        const studentDocRef = doc(db, "students", student.studentId);
+        return getDoc(studentDocRef);
+      }
+    );
+    const studentDocs = await Promise.all(studentPromises);
+    const studentData = studentDocs.map((studentDoc) => studentDoc.data());
+    return NextResponse.json(studentData);
+  } catch (error) {
+    console.error("There was an error getting student info", error);
+    return NextResponse.json(
+      { message: "Error getting student info" },
+      { status: 500 }
+    );
+  }
+}
